a85603458d3e8f522d4d11bc25e9d5435452f4c4
임현진 <hiimhoihoi@gmail.com>
2026-07-07 17:37:06 +0900

    [feat] 입소 데이터 로드 추가 및 입소 팝업 UIManager 연결
    - Admission JSON 데이터 로드 등록
    - 입소 대기자 리스트 팝업용 ViewModel 기본 구조 추가

---
+++ Assets/Scripts/ViewModel/Admission/AdmissionPopupViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+public class AdmissionPopupViewModel : ViewModelBase
+{
+    private readonly List<AdmissionData> _admissionDatas = new List<AdmissionData>();
+
+    public IReadOnlyList<AdmissionData> AdmissionDatas => _admissionDatas;
+    public AdmissionData SelectedAdmissionData { get; private set; }
+
+    public void Initialize()
+    {
+        _admissionDatas.Clear();
+
+        // TEST
+        AddAdmissionData("admission_01");
+        AddAdmissionData("admission_02");
+        AddAdmissionData("admission_03");
+    }
+
+    public void SelectAdmissionData(string id)
+    {
+        SelectedAdmissionData = null;
+
+        foreach (AdmissionData admissionData in _admissionDatas)
+        {
+            if (admissionData.ID == id)
+            {
+                SelectedAdmissionData = admissionData;
+                OnPropertyChanged(nameof(SelectedAdmissionData));
+                return;
+            }
+        }
+    }
+
+    private void AddAdmissionData(string id)
+    {
+        AdmissionData admissionData = GameDataManager.Inst.GetData<AdmissionData>(id);
+
+        if (admissionData == null)
+        {
+            return;
+        }
+
+        _admissionDatas.Add(admissionData);
+    }
+}
