33e35059701b939825977cb126b4798f4b5ece8f
김성광 <rweqtwed@gmail.com>
2026-07-07 15:06:35 +0900

    [feat] tycoon construct step 0 - Room 저장 구조 & 런타임 그리드 & MVVM 기초

---
+++ Assets/Scripts/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+
+// 뷰모델 공통 베이스, 강사님꺼 가지고 옴
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
