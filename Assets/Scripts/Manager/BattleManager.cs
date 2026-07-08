f1431477403affcfd6408817b528583b7eac48bb
최진영 <jychoi194@gmail.com>
2026-07-08 13:00:56 +0900

    [feat] 액션 큐 자료구조(BattleActionModel, BattleManager)추가 및 배틀매니저 오브젝트 씬에 추가

---
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//전투 라운드의 액션 큐를 생성하고 전체 진행을 관리하는 매니저
+public class BattleManager : SingletonBase<BattleManager>
+{
+    private Queue<BattleActionModel> _actionQueue = new Queue<BattleActionModel>();
+
+    //턴 순서 리스트를 받아와 각 유닛을 큐에 넣고, 마지막에 플레이어 개입 액션을 추가한다
+    public void BuildActionQueue(List<BattleUnitModel> turnOrder)
+    {
+        _actionQueue.Clear();
+
+        foreach (BattleUnitModel unit in turnOrder)
+        {
+            BattleActionModel unitAction = new BattleActionModel();
+            unitAction.Unit = unit;
+            unitAction.IsPlayerAction = false;
+
+            _actionQueue.Enqueue(unitAction);
+        }
+
+        BattleActionModel playerAction = new BattleActionModel();
+        playerAction.Unit = null;
+        playerAction.IsPlayerAction = true;
+
+        _actionQueue.Enqueue(playerAction);
+    }
+
+    public bool HasNextAction()
+    {
+        return _actionQueue.Count > 0;
+    }
+
+    public BattleActionModel GetNextAction()
+    {
+        if (_actionQueue.Count == 0)
+        {
+            return null;
+        }
+
+        return _actionQueue.Dequeue();
+    }
+}
---
+++ Assets/Scripts/Model/BattleActionModel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+//전투 액션 큐에 들어가는 개별 행동 정보를 저장하는 모델
+public class BattleActionModel
+{
+    public BattleUnitModel Unit { get; set; } //행동 주체 받아옴(영웅/적), 플레이어 개입 액션은 null (playerAction.Unit = null;)
+
+    public bool IsPlayerAction { get; set;} //true면 플레이어(요양사) 개입 액션
+}
