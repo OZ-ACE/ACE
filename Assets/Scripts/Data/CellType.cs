07fd16829a36b8c388b21bc069618a6b79395228
Selena (Jinyoung Choi) <jychoi194@gmail.com>
2026-07-08 18:01:57 +0900

    Revert "[construct](김성광) 건설 시스템 데이터/구조 구현 완료"

--- Assets/Scripts/Data/CellType.cs
+++
@@ -1,5 +0,0 @@
-﻿public enum CellType
-{
-    Sky,
-    Earth,
-}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,10 +2,5 @@
 
 public class GameManager : SingletonBase<GameManager>
 {
-    public string CurrentDialogueID { get; private set; } = "Opening_01";
 
-    public void SetDialogueID(string dialogueID)
-    {
-        CurrentDialogueID = dialogueID;
-    }
 }
