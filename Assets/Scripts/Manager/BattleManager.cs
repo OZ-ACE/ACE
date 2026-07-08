20df75368bb382ffbebbc66f1fcee0a4b37f4d2a
최진영 <jychoi194@gmail.com>
2026-07-08 14:34:15 +0900

    [feat] 플레이어 개입 턴 결과 처리 로직 추가 (BattleActionResult, 에너지 게이지)

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 
 //전투 라운드의 액션 큐를 생성하고 전체 진행을 관리하는 매니저
 public class BattleManager : SingletonBase<BattleManager>
 {
+    private const int MaxEnergyGauge = 3; //플레이어 개입 턴의 에너지 게이지 총량
+
     private Queue<BattleActionModel> _actionQueue = new Queue<BattleActionModel>();
+    private int _energyGauge;
 
     //턴 순서 리스트를 받아와 각 유닛을 큐에 넣고, 마지막에 플레이어 개입 액션을 추가한다
     public void BuildActionQueue(List<BattleUnitModel> turnOrder)
     {
         _actionQueue.Clear();
+        _energyGauge = MaxEnergyGauge;
 
         foreach (BattleUnitModel unit in turnOrder)
         {
@@ -25,6 +28,33 @@ public class BattleManager : SingletonBase<BattleManager>
         playerAction.IsPlayerAction = true;
 
         _actionQueue.Enqueue(playerAction);
+    }
+
+    //큐 안에서 targetUnitId를 가진 행동을 찾아 결과를 확정한다. 에너지 부족 시 선택 자체가 불가능하도록 UI에서 막을 예정이라, 여기서의 false 반환은 그 상황이 뚫렸을 때를 대비한 방어 코드이다
+    public bool SetActionResult(string targetUnitId, BattleActionResult result, int energyCost)
+    {
+        if (_energyGauge < energyCost)
+        {
+            return false;
+        }
+
+        foreach (BattleActionModel action in _actionQueue)
+        {
+            if (action.Unit != null && action.Unit.ID == targetUnitId)
+            {
+                action.Result = result;
+                _energyGauge -= energyCost;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int GetRemainingEnergy()
+    {
+        return _energyGauge;
     }
 
     public bool HasNextAction()
--- Assets/Scripts/Model/BattleActionModel.cs
+++ Assets/Scripts/Model/BattleActionModel.cs
@@ -6,4 +6,6 @@ public class BattleActionModel
     public BattleUnitModel Unit { get; set; } //행동 주체 받아옴(영웅/적), 플레이어 개입 액션은 null (playerAction.Unit = null;)
 
     public bool IsPlayerAction { get; set;} //true면 플레이어(요양사) 개입 액션
+
+    public BattleActionResult Result { get; set; } = BattleActionResult.None; //개입 턴 후 처리결과. 기본값 미확정none
 }
