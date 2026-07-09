fdd30983f651afee589ba244d1abbaf4f8423b23
이주현 <juhyun040100@gmail.com>
2026-07-09 15:18:54 +0900

    [feat] 로딩 UI 제작 및 데이터 테이블 작성

---
+++ Assets/Scripts/UI/Title/LoadingUI.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class LoadingUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Text_Loading;
+
+    private void OnEnable()
+    {
+        Text_Loading.text = SetLoadingText();
+    }
+
+    private string SetLoadingText()
+    {
+        string loadingText = "";
+
+        return loadingText;
+    }
+}
