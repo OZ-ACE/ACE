86429f288c3003221e5ea5c6f5fa8a3203d52ba5
임현진 <hiimhoihoi@gmail.com>
2026-07-10 17:01:20 +0900

    [feat] 입소 신청서 팝업 구현
    - Pick 버튼과 입소 신청서 팝업 연결
    - 종이 넘기기/되돌리기 애니메이션 구현
    - 입소 신청서 UI 및 프리팹 추가
    - AdmissionManager와 팝업 연동

---
+++ Assets/Scripts/ViewModel/AdmissionPopupViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+public class AdmissionPopupViewModel : ViewModelBase
+{
+    private readonly List<AdmissionCandidateModel> _candidateModels = new List<AdmissionCandidateModel>();
+
+    public IReadOnlyList<AdmissionCandidateModel> CandidateModels => _candidateModels;
+    public AdmissionCandidateModel SelectedCandidateModel { get; private set; }
+
+    public void Initialize()
+    {
+        _candidateModels.Clear();
+
+        List<AdmissionCandidateModel> candidateModels = AdmissionManager.Inst.GetCandidateModels();
+
+        for (int i = 0; i < candidateModels.Count; i++)
+        {
+            _candidateModels.Add(candidateModels[i]);
+        }
+    }
+
+    public HeroData GetHeroData(int index)
+    {
+        if (index < 0 || index >= _candidateModels.Count)
+        {
+            return null;
+        }
+
+        return GameDataManager.Inst.GetData<HeroData>(_candidateModels[index].HeroId);
+    }
+
+    public void SelectCandidateModel(int candidateId)
+    {
+        SelectedCandidateModel = null;
+
+        for (int i = 0; i < _candidateModels.Count; i++)
+        {
+            if (_candidateModels[i].CandidateId == candidateId)
+            {
+                SelectedCandidateModel = _candidateModels[i];
+                OnPropertyChanged(nameof(SelectedCandidateModel));
+                return;
+            }
+        }
+    }
+}
