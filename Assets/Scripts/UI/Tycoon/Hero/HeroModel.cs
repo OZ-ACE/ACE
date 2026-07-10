b610d947b2b2c55afd497ce4fbd9e59644358a79
이주현 <juhyun040100@gmail.com>
2026-07-10 18:00:59 +0900

    [feat] 영웅 스테이터스 창 구현

--- Assets/Scripts/UI/Tycoon/Hero/HeroModel.cs
+++ Assets/Scripts/UI/Tycoon/Hero/HeroModel.cs
@@ -2,6 +2,34 @@
 
 public class HeroModel
 {
+    public string HeroID;
+    public string Name;
+    public string Description;
+    public string DiseaseName;
+    public int Age;
+    public string Skill;
+
     public int Affection;
     public int Satisfaction;
+
+    public void LoadHeroData(string heroID)
+    {
+        var heroData = GameDataManager.Inst.GetData<HeroBasic>(heroID);
+
+        HeroID = heroID;
+        Name = heroData.Name;
+        Description = heroData.Description;
+        DiseaseName = heroData.DiseaseName;
+        Age = heroData.Age;
+        Skill = heroData.Skill;
+
+        Affection = SaveManager.Inst.CurrentPlayerModel.Hero.Affection;
+        Satisfaction = SaveManager.Inst.CurrentPlayerModel.Hero.Satisfaction;
+    }
+
+    public void SaveHeroProgress()
+    {
+        SaveManager.Inst.CurrentPlayerModel.Hero.Affection = Affection;
+        SaveManager.Inst.CurrentPlayerModel.Hero.Satisfaction = Satisfaction;
+    }
 }
---
+++ Assets/Scripts/UI/Tycoon/Hero/HeroViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Unity.VisualScripting;
+
+public class HeroViewModel : ViewModelBase
+{
+    private HeroModel _model;
+
+    private string _heroName;
+    public string HeroName => _heroName;
+
+    private string _description;
+    public string Description => _description;
+
+    private string _diseaseName;
+    public string DiseaseName => _diseaseName;
+
+    private string _age;
+    public string Age => _age;
+
+    private string _skill;
+    public string Skill => _skill;
+
+    private int _affection;
+    public int Affection
+    {
+        get => _affection;
+        set
+        {
+            if (_affection != value)
+            {
+                _affection = value;
+                OnPropertyChanged(nameof(Affection));
+            }
+        }
+    }
+
+    private int _satisfaction;
+    public int Satisfaction
+    {
+        get => _satisfaction;
+        set
+        {
+            if (_satisfaction != value)
+            {
+                _satisfaction = value;
+                OnPropertyChanged(nameof(Satisfaction));
+            }
+        }
+    }
+
+    public void Init(HeroModel model)
+    {
+        _model = model;
+
+        _heroName = _model.HeroName;
+        _description = _model.Description;
+        _diseaseName = _model.DiseaseName;
+        _age = _model.Age;
+        _skill = _model.Skill;
+        
+        _affection = _model.Affection;
+        _satisfaction = _model.Satisfaction;
+    }
+
+    public void InvokeOnceOnInit()
+    {
+        OnPropertyChanged(nameof(HeroName));
+        OnPropertyChanged(nameof(Description));
+        OnPropertyChanged(nameof(DiseaseName));
+        OnPropertyChanged(nameof(Age));
+        OnPropertyChanged(nameof(Skill));
+        OnPropertyChanged(nameof(Satisfaction));
+        OnPropertyChanged(nameof(Affection));
+    }
+
+    public void AddAffection(int amount)
+    {
+        Affection += amount;
+
+        _model.Affection = Affection;
+        _model.SaveHeroProgress();
+    }
+
+    public void AddSatisfaction(int amount)
+    {
+        Satisfaction += amount;
+
+        _model.Satisfaction = Satisfaction;
+        _model.SaveHeroProgress();
+    }
+}
