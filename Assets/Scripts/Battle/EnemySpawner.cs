73d2f3b66a0bb1721e34df2ec229b4e434ca76ec
MinGeon <goo9781@gmail.com>
2026-07-19 17:32:41 +0900

    [feat] Enemy 전투 프리팹 스폰 기능 추가
    - EnemySpawnRoot에 EnemySpawner 컴포넌트 추가
    - SpawnPoint 3개를 순회해 EnemyTemp 프리팹 생성
    - 생성된 Enemy를 EnemySpawnRoot 하위에 배치

---
+++ Assets/Scripts/Battle/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn")]
+    [SerializeField] private GameObject EnemyPrefab;
+    [SerializeField] private Transform EnemySpawnRoot;
+    [SerializeField] private Transform[] SpawnPoints;
+
+    public void SpawnEnemies()
+    {
+        if (EnemyPrefab == null || EnemySpawnRoot == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < SpawnPoints.Length; i++)
+        {
+            Transform spawnPoint = SpawnPoints[i];
+
+            if (spawnPoint == null)
+            {
+                continue;
+            }
+
+            Instantiate(
+                EnemyPrefab,
+                spawnPoint.position,
+                spawnPoint.rotation,
+                EnemySpawnRoot);
+        }
+    }
+}
