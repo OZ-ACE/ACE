eeb4a20026ee728dd1b0cd17f78ea17917355f33
이주현 <juhyun040100@gmail.com>
2026-07-19 17:27:28 +0900

    [feat] 사운드 매니저 구현

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,9 +1,48 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Audio;
 
 public class SoundManager : SingletonBase<SoundManager>
 {
     [SerializeField] private AudioMixer Audio_Mixer;
+    [SerializeField] private AudioSource Audio_BGM;
+    [SerializeField] private AudioSource Audio_SFX;
+
+    public async UniTaskVoid LoadAndPlayAudioClip(AudioSource audioSource, string audioPath, bool isLoop = false)
+    {
+        AudioClip clip = await ResourceManager.Inst.LoadAsset<AudioClip>(audioPath);
+
+        if (isLoop == true)
+        {
+            audioSource.clip = clip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayBGM(string clip)
+    {
+        LoadAndPlayAudioClip(Audio_BGM, clip, true).Forget();
+    }
+
+    public void PlaySFX(string clip)
+    {
+        LoadAndPlayAudioClip(Audio_BGM, clip).Forget();
+    }
+
+    public void StopBGM()
+    {
+        Audio_BGM.Stop();
+    }
+
+    public void StopSFX()
+    {
+        Audio_SFX.Stop();
+    }
 
     public AudioMixer GetAudioMixer()
     {
