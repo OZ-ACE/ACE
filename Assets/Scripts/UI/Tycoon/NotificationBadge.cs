eae7cf8260541f5bdbd24f3a6dcb8dffdf212164
임현진 <hiimhoihoi@gmail.com>
2026-07-26 17:48:04 +0900

    [feat] 퀘스트 알림 뱃지 기능 추가
    - 메인 퀘스트 달성 시 알림 뱃지 표시 기능 추가
    - 돌발 퀘스트 진행 상태 알림 뱃지 추가
    - 퀘스트 탭 진입 시 일반 알림 뱃지 해제
    - 퀘스트/돌발 퀘스트 알림 우선순위 처리
    - NotificationBadge UI 연동

---
+++ Assets/Scripts/UI/Tycoon/NotificationBadge.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum NotificationType
+{
+    None = 0,
+    New,
+    HeroRequest,
+}
+
+public class NotificationBadge : MonoBehaviour
+{
+    [SerializeField] private GameObject GameObject_Badge;
+    [SerializeField] private Image Image_Icon;
+
+    [SerializeField] private Sprite Sprite_New;
+    [SerializeField] private Color Color_New = new Color32(0xE7, 0xB9, 0x5C, 0xFF);
+    
+    [SerializeField] private Sprite Sprite_HeroRequest;
+
+    public void SetNotification(NotificationType type)
+    {
+        switch (type)
+        {
+            case NotificationType.None:
+                GameObject_Badge.SetActive(false);
+                break;
+
+            case NotificationType.New:
+                GameObject_Badge.SetActive(true);
+                Image_Icon.sprite = Sprite_New;
+                Image_Icon.color = Color_New;
+                break;
+
+            case NotificationType.HeroRequest:
+                GameObject_Badge.SetActive(true);
+                Image_Icon.sprite = Sprite_HeroRequest;
+                break;
+        }
+    }
+}
